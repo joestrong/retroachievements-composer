ebb2998f79f3bb4bd20add5d1bc39eb91ba31fe8
Joe Strong <joestrong@gmail.com>
2017-10-12 19:40:31 +0100

    Made code testable by using a mocked Guzzle object

--- spec/RetroAchievementsSpec.php
+++ spec/RetroAchievementsSpec.php
@@ -2,7 +2,12 @@
 
 namespace spec\JoeStrong\RetroAchievements;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use JoeStrong\RetroAchievements\RetroAchievements;
+use JoeStrong\RetroAchievements\User;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,7 +15,7 @@ class RetroAchievementsSpec extends ObjectBehavior
 {
     function let()
     {
-        $this->beConstructedWith('', '');
+        $this->beConstructedWith('user', 'apiKey', new Client());
     }
 
     function it_is_initializable()
@@ -20,16 +25,49 @@ class RetroAchievementsSpec extends ObjectBehavior
 
     function it_should_get_top_ten_users()
     {
+        $userOb = (object) [
+            1 => 'username',
+            2 => 500,
+            3 => 400
+        ];
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(array_fill(0, 10, $userOb))),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
+
         $this->getTopTenUsers()->shouldHaveCount(10);
     }
 
     function it_should_get_console_ids()
     {
+        $userOb = (object) [
+            'ID' => 3,
+            'Name' => 'Super Nintendo Entertainment System',
+        ];
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(array_fill(0, 10, $userOb))),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
+
         $this->getConsoles();
     }
 
     function it_should_get_games_list()
     {
+        $userOb = (object) [
+            'ID' => 1,
+            'Title' => 'Super Mario World',
+            'ConsoleID' => 3,
+            'ImageIcon' => '',
+        ];
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(array_fill(0, 10, $userOb))),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
+
         $consoleId = 1;
         $this->getGamesForConsole($consoleId);
     }
