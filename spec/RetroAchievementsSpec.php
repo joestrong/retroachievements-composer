de6d474e4b24b19995d6920918689b45068d9270
Joe Strong <joestrong@gmail.com>
2017-10-13 00:28:13 +0100

    Updated tests to check return types, and for thrown exceptions on API 404s

--- spec/RetroAchievementsSpec.php
+++ spec/RetroAchievementsSpec.php
@@ -3,9 +3,12 @@
 namespace spec\JoeStrong\RetroAchievements;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+use JoeStrong\RetroAchievements\Console;
+use JoeStrong\RetroAchievements\Game;
 use JoeStrong\RetroAchievements\RetroAchievements;
 use JoeStrong\RetroAchievements\User;
 use PhpSpec\ObjectBehavior;
@@ -39,7 +42,34 @@ class RetroAchievementsSpec extends ObjectBehavior
         $this->getTopTenUsers()->shouldHaveCount(10);
     }
 
-    function it_should_get_console_ids()
+    function it_should_get_top_ten_users_using_user_object()
+    {
+        $userOb = (object) [
+            1 => 'username',
+            2 => 500,
+            3 => 400
+        ];
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(array_fill(0, 10, $userOb))),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
+
+        $this->getTopTenUsers()->shouldOnlyContainInstancesOf(User::class);
+    }
+
+    function it_should_throw_errors_when_api_offline()
+    {
+        $mock = new MockHandler([
+            new Response(404),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
+
+        $this->shouldThrow(ClientException::class)->duringGetTopTenUsers();
+    }
+
+    function it_should_get_consoles()
     {
         $userOb = (object) [
             'ID' => 3,
@@ -51,10 +81,10 @@ class RetroAchievementsSpec extends ObjectBehavior
         $handler = HandlerStack::create($mock);
         $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
 
-        $this->getConsoles();
+        $this->getConsoles()->shouldOnlyContainInstancesOf(Console::class);
     }
 
-    function it_should_get_games_list()
+    function it_should_get_a_list_of_games_for_a_console()
     {
         $userOb = (object) [
             'ID' => 1,
@@ -69,6 +99,23 @@ class RetroAchievementsSpec extends ObjectBehavior
         $this->beConstructedWith('user', 'apiKey', new Client(['handler' => $handler]));
 
         $consoleId = 1;
-        $this->getGamesForConsole($consoleId);
+        $this->getGamesForConsole($consoleId)->shouldOnlyContainInstancesOf(Game::class);
+    }
+
+    public function getMatchers(): array
+    {
+        return [
+            'onlyContainInstancesOf' => function ($subject, $key) {
+                if (!is_iterable($subject)) {
+                    return false;
+                }
+                foreach ($subject as $item) {
+                    if (!$item instanceof $key) {
+                        return false;
+                    }
+                }
+                return true;
+            },
+        ];
     }
 }
